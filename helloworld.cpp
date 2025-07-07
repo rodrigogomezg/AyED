49138d4cf05cc4d9842ff0f4042b64139054650a
Rodrigo Gomez <rodrigogomezrgg@gmail.com>
2025-07-06 23:00:06 -0300

    .

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hola Rodrigo Gomez!"};
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+    cout << "Hola Rodrigo Gomez! << endl;
+    return 0;
 }
